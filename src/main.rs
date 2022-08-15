80bc5046cacec29965ca67d8d4a226f65e0e95b1
Jonny Broquist <jonathan@jbro.io>
2022-08-15 10:59:43 -0700

    refactor: simplify command match statement

--- src/main.rs
+++ src/main.rs
@@ -48,12 +48,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     match &args.command {
-        Some(Commands::Init) => {
-            init();
-        }
-        Some(Commands::Run) => {
-            tracker::run_tracker().await?;
-        }
+        Some(Commands::Init) => init(),
+        Some(Commands::Run) | None => tracker::run_tracker().await?,
         Some(Commands::Add { coin_id, position }) => {
             println!("Adding coin: {:?} {:?}", coin_id, position);
             add_coin(coin_id, position);
@@ -70,7 +66,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             println!("Listing all the coins...");
             list_all_coins();
         }
-        None => tracker::run_tracker().await?,
     }
 
     Ok(())
